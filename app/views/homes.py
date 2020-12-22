afd5586498c90e9ac0ec8e21834e0e5c87a25d46
Tran Duc Y <ducyk41cntt@gmail.com>
2020-12-22 15:43:45 +0700

    cdn templete

--- app/views/homes.py
+++ app/views/homes.py
@@ -3,21 +3,35 @@ from django.http import HttpResponse
 import datetime
 
 def index(request):
-
-    return render(request, 'app/index.html')
+    context = {
+        "menu_active": 0
+    }
+    return render(request, 'app/index.html', context)
 
 def category(request):
-    return render(request, 'app/category.html')
+    context = {
+        "menu_active": 1
+    }
+    return render(request, 'app/category.html', context)
 
 def cart(request):
     return render(request, 'app/cart.html')
 
 def login(request):
-    return render(request, 'app/login.html')
+    context = {
+        "menu_active": 3
+    }
+    return render(request, 'app/login.html', context)
 
 def register(request):
-    return render(request, 'app/register.html')
+    context = {
+        "menu_active": 3
+    }
+    return render(request, 'app/register.html', context)
 
 def detail(request):
-    return render(request, 'app/single-product.html')
+    context = {
+        "menu_active": 1
+    }
+    return render(request, 'app/single-product.html', context)
 
